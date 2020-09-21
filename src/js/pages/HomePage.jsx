b68709886e8d8acf5539c94b09bd1926bf176f03
Phanu <phanuelgadenne@yahoo.fr>
2020-09-21 14:42:05 +0200

    modif HomePage

--- src/js/pages/HomePage.jsx
+++ src/js/pages/HomePage.jsx
@@ -4,21 +4,8 @@ import { NavLink } from "react-router-dom";
 const HomePage = (props) => {
   return (
     <div className="jumbotron">
-      <h1 className="display-3">Hello, world!</h1>
-      <p className="lead">
-        This is a simple hero unit, a simple jumbotron-style component for
-        calling extra attention to featured content or information.
-      </p>
-      <hr className="my-4" />
-      <p>
-        It uses utility classes for typography and spacing to space content out
-        within the larger container.
-      </p>
-      <p className="lead">
-        <NavLink className="btn btn-primary btn-lg" to="#" role="button">
-          Learn more
-        </NavLink>
-      </p>
+      <h1 className="display-3">Hello, team!</h1>
+      <p>WELCOME SUR MON APPLI SYMFONY APIPLATFORM REACT !!!</p>
     </div>
   );
 };
